39fcfa7313bbc37257b8649b894c19d9d943f23b
Lorennie <lorraine.rennie@landregistry.gsi.gov.uk>
2015-11-30 11:16:30 +0000

    Update get_json.rb

--- tests/JSON/features/step_definitions/get_json.rb
+++ tests/JSON/features/step_definitions/get_json.rb
@@ -11,7 +11,7 @@
     #This is returning the first level data
   puts $structure_hash['title_number'] + ' - title_number'
   puts $structure_hash['tenure'] + ' - tenure'
-  puts $structure_hash['entries'][12]['entry_id'] + ' - entry_id'
+  puts $structure_hash['entries'][12]['entry_id'] + ' - entry_id' # this is not entry no 12 but no 13
   puts $structure_hash['application_reference'] + ' - application_reference'
   puts $structure_hash['migration_errors'][0]['extractor'] + ' - extractor error'
   puts $structure_hash['entries'][16]['sub_register'] + ' - sub_register'
@@ -26,7 +26,7 @@
    expect($structure_hash['entries'][12]['entry_id']).to eq('2006-09-06 11:32:13.352265')
    expect($structure_hash['application_reference']).to eq('E006DWW')
    expect($structure_hash['migration_errors'][0]['extractor']).to eq('Register Check')
-   expect($structure_hash['entries'][16]['sub_register']).to eq('S')
+   expect($structure_hash['entries'][16]['sub_register']).to eq('S')LIkewise not entry no 16 but 17
    expect($structure_hash['edition_date']).to eq('2006-09-06')
    expect($structure_hash['entries'][0]['status']).to eq('Current')
 
